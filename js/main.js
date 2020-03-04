023c816ba4671a255da76f3eaa14b76b9865eb3b
Arkady Pugachev <qweeex@yandex.com>
2020-03-04 17:31:01 +0400

    30%

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(() => {
 
-
+    // Massages slider
+    let massages = new Swiper('.massages-slider', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 
 });
